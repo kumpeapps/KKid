c51096e29b69a203c470a6ca545b67e440d73dd7
Justin Kumpe <jakumpe@kumpes.com>
2020-08-28 14:05:40 -0500

    Initial Commit

---
+++ KKid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KKid
+//
+//  Created by Justin Kumpe on 8/28/20.
+//  Copyright © 2020 Justin Kumpe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
