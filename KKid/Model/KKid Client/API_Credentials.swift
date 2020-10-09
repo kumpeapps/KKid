ce3f0a46908eb0e936f8314679cba8009854c324
justinkumpe <37677675+justinkumpe@users.noreply.github.com>
2020-10-08 20:42:57 -0500

    Create API_Credentials.swift

---
+++ KKid/Model/KKid Client/API_Credentials.swift
@@ -0,0 +1,21 @@
+//
+//  Credentials.swift
+//  KKid
+//
+//  Created by Justin Kumpe on 9/26/20.
+//  Copyright © 2020 Justin Kumpe. All rights reserved.
+//
+
+import Foundation
+
+struct APICredentials{
+    let kkid: KKid
+    
+//    MARK: KKid Credentials
+    struct KKid{
+        #error("API Credentials Required")
+        static let username = ""
+        static let apiPassword = ""
+    }
+    
+}
