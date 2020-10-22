818e3a363f8b3d236ff9299351c9e51d9e856446
Justin Kumpe <jakumpe@kumpes.com>
2020-10-21 21:03:57 -0500

    migrated dayAsNumber to API Pull

--- KKid/AppDelegate.swift
+++ KKid/AppDelegate.swift
@@ -21,6 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         checkIfFirstLaunch()
+        
+        DebugHelpers.logDocumentsUrl()
 
 //        Configure Google AdMob
         GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers =
