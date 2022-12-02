b1691944b359001aaf2c303ed4febc2643b9ca31
Justin Kumpe <jakumpe@kumpes.com>
2022-12-02 15:11:56 -0600

    updated screen shots

--- KKidUITests/KKidUITests.swift
+++ KKidUITests/KKidUITests.swift
@@ -207,12 +207,12 @@ class KKidUITests: XCTestCase {
     }
 
     func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+        /*if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
             }
-        }
+        }*/
     }
 
     func tapCoordinate(at xCoordinate: Double, and yCoordinate: Double) {
